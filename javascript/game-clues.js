1fe79ba5bcd79b0e68dcd7f78e350310d147d804
Nate Gay <nate@alifeofdanger.com>
2011-08-31 16:20:06 -0400

    testing clue flagging

--- javascript/game-clues.js
+++ javascript/game-clues.js
@@ -27,8 +27,7 @@ function betweenClue() {
 	}
 	else {
 		clueNum++;
-		$(".horizontalClueArea").append('<div class="horizontalClue clue' + clueNum + '"><div class="betweenClue" style="background-image:url(' + resources + 'clues/between.gif);"></div><div style="background-image:url(' + resources + 'row' + clueRow.left + '/' + clueLeft + '.jpg);" class="clue"></div><div style="background-image:url(' + resources + 'row' + clueRow.center + '/' + clueCenter + '.jpg);" class="clue"></div><div style="background-image:url(' + resources + 'row' + clueRow.right + '/' + clueRight + '.jpg);" class="clue"></div></div>');
-		clues.between.push([clueRow.left, clueLeft, clueRow.center, clueCenter, clueRow.right, clueRight]);
+		$(".horizontalClueArea").append('<div class="horizontalClue clue' + clueNum + '"><div class="betweenClue clue' + clueNum + '" style="background-image:url(' + resources + 'clues/between.gif);"></div><div style="background-image:url(' + resources + 'row' + clueRow.left + '/' + clueLeft + '.jpg);" class="clue"></div><div style="background-image:url(' + resources + 'row' + clueRow.center + '/' + clueCenter + '.jpg);" class="clue"></div><div style="background-image:url(' + resources + 'row' + clueRow.right + '/' + clueRight + '.jpg);" class="clue"></div></div>');
 		$("#clue" + clueNum).rightClick( function(e) {
 			if (this.hasClass("flagged") == true) {
 				$(this).removeClass("flagged");
@@ -37,6 +36,7 @@ function betweenClue() {
 				$(this).addClass("flagged");
 			};
 		});
+		clues.between.push([clueRow.left, clueLeft, clueRow.center, clueCenter, clueRow.right, clueRight]);
 //		checkPossibilities();
 	};
 };
